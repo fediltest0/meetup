885cd88abe3ca92b5ff7e4da2fb352bc0c323e01
fediltest0 <fed.il.test0@gmail.com>
2015-11-17 09:35:14 +0000

    chore(jscs) add JSCS validation
    Add .jscsrc file with set of validation rules and run validation during build test step
    
    fix #2

--- Gruntfile.js
+++ Gruntfile.js
@@ -12,14 +12,22 @@ module.exports = function(grunt) {
                     src: ['Gruntfile.js']
                 }
             }
+        },
+        jscs: {
+            src: "**.js",
+            options: {
+                config: ".jscsrc",
+                verbose: true
+            }
         }
     });
     
     // Load the plugin that provides the "uglify" task.
     grunt.loadNpmTasks('grunt-contrib-jshint');
+    grunt.loadNpmTasks('grunt-jscs');
     
     // Custom tasks
-    grunt.registerTask('test', ['jshint']);
+    grunt.registerTask('test', ['jshint', 'jscs']);
     
     // Default task.
     grunt.registerTask('default', ['test']);
