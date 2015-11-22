78f436b6f634a070b17634493475d1a77cedfdb0
fediltest0 <fed.il.test0@gmail.com>
2015-11-22 02:13:35 +0000

    fix(test) fix e2e test to use new protractor API

--- test/app/e2e/spec.js
+++ test/app/e2e/spec.js
@@ -1,10 +1,5 @@
-describe('Protractor Demo App', function() {
-    var ptor;
+describe('Front-End.IL Meetup Demo App', function() {
 
-    beforeEach(function() {
-        ptor = protractor.getInstance();
-    });
-    
     it('should have a title', function() {
         browser.get('http://localhost:8282/app/index.html');
         expect(browser.getTitle()).toEqual('Front-End.IL');
@@ -13,8 +8,7 @@ describe('Protractor Demo App', function() {
     it('should open login page by default', function() {
         var url;
         browser.get('http://localhost:8282/app/index.html');
-        ptor.waitForAngular();
-        url = ptor.getCurrentUrl();
+        url = browser.getLocationAbsUrl();
         expect(url.indexOf('#/login') > 0).toBeTruthy();
     });
 
