807b74dd4a345ed61e061b8025b4e1633b46c407
Travis CI <travis@localhost.localdomain>
2015-11-22 03:34:21 +0000

    Generate gh-pages with Grunt task gh-pages:dist

---
+++ docs/js/docs-setup.js
@@ -0,0 +1,99 @@
+NG_DOCS={
+  "sections": {
+    "tutorial": "Tutorial",
+    "api": "API Documentation"
+  },
+  "pages": [
+    {
+      "section": "tutorial",
+      "id": "index",
+      "shortName": "Tutorial",
+      "type": "overview",
+      "moduleName": "Tutorial",
+      "shortDescription": "",
+      "keywords": "overview tutorial"
+    },
+    {
+      "section": "tutorial",
+      "id": "step_00",
+      "shortName": "0 - Repository",
+      "type": "overview",
+      "moduleName": "0 - Repository",
+      "shortDescription": "",
+      "keywords": "doc-tutorial-nav overview repository tutorial"
+    },
+    {
+      "section": "tutorial",
+      "id": "step_01",
+      "shortName": "1 - Web IDE",
+      "type": "overview",
+      "moduleName": "1 - Web IDE",
+      "shortDescription": "",
+      "keywords": "doc-tutorial-nav ide overview tutorial web"
+    },
+    {
+      "section": "tutorial",
+      "id": "step_02",
+      "shortName": "2 - Task Runner",
+      "type": "overview",
+      "moduleName": "2 - Task Runner",
+      "shortDescription": "",
+      "keywords": "doc-tutorial-nav overview runner task tutorial"
+    },
+    {
+      "section": "api",
+      "id": "index",
+      "shortName": "API Reference",
+      "type": "overview",
+      "moduleName": "API Reference",
+      "shortDescription": "Front-End.IL Meetup API Docs",
+      "keywords": "api application cloud created demo demonstrate development docs end-to-end front-end guide il intend meetup overview process reference steps"
+    },
+    {
+      "section": "api",
+      "id": "app.controller:LoginController",
+      "shortName": "LoginController",
+      "type": "controller",
+      "moduleName": "app",
+      "shortDescription": "The LoginContoller is a controller that is used to expose",
+      "keywords": "$firebaeauth $location $log account api app authenticated authorises controller credentials expose function functionality github google html login logincontoller logincontroller methods multiple ng-click ng-controller oauth povide provider refservice required user utilised view vm withgithub withgoogle"
+    },
+    {
+      "section": "api",
+      "id": "app.controller:MembersController",
+      "shortName": "MembersController",
+      "type": "controller",
+      "moduleName": "app",
+      "shortDescription": "The MembersController is a controller that is used to expose",
+      "keywords": "$mdsidenav $routeparams $scope active api app changes controller details expose function functionality html manage md-component-id meetupservice member members memberscontroller menu menuid methods navigation ng-click ng-controller parameters persisting required responsible route selected service sidebar toggled toggles togglesidenav ui updating user vm watching"
+    },
+    {
+      "section": "api",
+      "id": "app.meetupService",
+      "shortName": "app.meetupService",
+      "type": "service",
+      "moduleName": "app",
+      "shortDescription": "The meetupService provides abstraction on data source and allows",
+      "keywords": "$firebasearray $id abstraction access allows api app asynchronously aviv basic bio called changed characters city cloned constant data default deleted doe don empty function getmember getmembers heavily integer internally john js list low-level mandatory meetup meetupservice member members method nophotourl notes object operations optional parameter parsed pass passed perform picture populated presance properties property received refservice relies representing returns service source starting string substitutes tel time updatemember"
+    },
+    {
+      "section": "api",
+      "id": "app.refService",
+      "shortName": "app.refService",
+      "type": "service",
+      "moduleName": "app",
+      "shortDescription": "The refService is used to initialise and cache Firebase reference",
+      "keywords": "$window abstraction access add api app application basefirebaseurl cache cached case cases collection controllers created data directives directly firebase function getref high-level html https initialise js low-level meetupservice object objects provide reference refservice requested returns root semantics service services source specific time"
+    }
+  ],
+  "apis": {
+    "tutorial": false,
+    "api": true
+  },
+  "html5Mode": false,
+  "editExample": true,
+  "startPage": "/api",
+  "scripts": [
+    "angular.min.js"
+  ]
+};
