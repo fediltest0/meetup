2382a51cb899aeb3230d6ec41961c0e199fafd88
fediltest0 <fed.il.test0@gmail.com>
2015-11-22 03:27:10 +0000

    chore(gh-pages) play with configuration

--- .initconfig/gh-pages.js
+++ .initconfig/gh-pages.js
@@ -3,16 +3,16 @@ module.exports = function(grunt) {
     return {
         dist: {
             options: {
-                // repo: grunt.config.process('<%= pkg.repo.url %>').replace(
-                //     /.*:\/\//,
-                //     'https://' + process.env.GH_TOKEN + '@'
-                // ),
+                repo: grunt.config.process('<%= pkg.repo.url %>').replace(
+                    /.*:\/\//,
+                    'https://' + process.env.GH_TOKEN + '@'
+                ),
                 base: 'dist',
                 message: 'Generate gh-pages with Grunt task gh-pages:dist',
-                // user: {
-                //     name: 'Travis CI',
-                //     email: 'travis@localhost.localdomain'
-                // },
+                user: {
+                    name: 'Travis CI',
+                    email: 'travis@localhost.localdomain'
+                },
                 silent: true
             },
             src: ['**']
